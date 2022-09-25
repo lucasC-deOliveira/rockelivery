0f1c167649f8931faca732fbc5ba340411a7f3aa
Lucas Carvalho <67061874+lucasC-deOliveira@users.noreply.github.com>
2022-09-25 08:56:45 -0300

    Criando a rota de deleção de usuários

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -3,7 +3,11 @@ defmodule Rockelivery do
 
   alias Rockelivery.Users.Get, as: UserGet
 
+  alias Rockelivery.Users.Delete, as: UserDelete
+
   defdelegate create_user(params), to: UserCreate, as: :call
 
   defdelegate get_user_by_id(id), to: UserGet, as: :by_id
+
+  defdelegate delete_user(id), to: UserDelete, as: :call
 end
