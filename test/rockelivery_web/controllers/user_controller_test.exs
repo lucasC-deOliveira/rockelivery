cc7a9a70b0907d99786352026d5d6a701457f202
Lucas Carvalho <67061874+lucasC-deOliveira@users.noreply.github.com>
2022-10-01 17:20:56 -0300

    Testando nosso controller pt 2

--- test/rockelivery_web/controllers/user_controller_test.exs
+++ test/rockelivery_web/controllers/user_controller_test.exs
@@ -56,4 +56,18 @@ defmodule RockeliveryWeb.UsersControllerTest do
       assert expected_response == response
     end
   end
+
+  describe "delete/2" do
+    test "when there is a user with the given id, deletes the user", %{conn: conn} do
+      id = "0344175f-863d-4067-a266-b851755d3ef9"
+      insert(:user)
+
+      response =
+        conn
+        |> delete(Routes.users_path(conn, :delete, id))
+        |> response(:no_content)
+
+        assert response == ""
+    end
+  end
 end
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,5 +1,7 @@
 defmodule Rockelivery.Factory do
-  use ExMachina
+  use ExMachina.Ecto, repo: Rockelivery.Repo
+
+  alias Rockelivery.User
 
   def user_params_factory do
     %{
@@ -12,5 +14,18 @@ defmodule Rockelivery.Factory do
       password: "123456",
    }
 
+  end
+
+  def user_factory do
+    %User{
+      age: 22,
+      address: "Rua dos ipes",
+      cep: "12345678",
+      name: "Lucas",
+      cpf: "12345678901",
+      email: "lucas@gmail.com",
+      password: "123456",
+      id: "0344175f-863d-4067-a266-b851755d3ef9"
+    }
   end
 end
