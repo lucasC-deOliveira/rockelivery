7b9bd62730100915e7c07b17162d123be3215429
Lucas Carvalho <67061874+lucasC-deOliveira@users.noreply.github.com>
2022-10-03 09:31:06 -0300

    Criando o módulo de criação de Pedido pt 3

--- lib/rockelivery/order.ex
+++ lib/rockelivery/order.ex
@@ -9,6 +9,8 @@ defmodule Rockelivery.Order do
 
   @primary_key {:id, :binary_id, autogenerate: true}
 
+  @foreign_key_type :binary_id
+
   @required_params [:address, :comments, :payment_method, :user_id]
 
   @derive {Jason.Encoder, only: @required_params ++ [:id]}
