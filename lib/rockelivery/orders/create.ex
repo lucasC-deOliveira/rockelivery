1ca5f09846884903d62dcf32dc70432c940410c7
Lucas Carvalho <67061874+lucasC-deOliveira@users.noreply.github.com>
2022-10-03 09:08:48 -0300

    Criando o módulo de criação de Pedido pt 2

--- lib/rockelivery/orders/create.ex
+++ lib/rockelivery/orders/create.ex
@@ -1,7 +1,7 @@
 defmodule Rockelivery.Order.Create do
   import Ecto.Query
 
-  alias Rockelivery.{Error, Repo,Item, Order}
+  alias Rockelivery.{Error, Repo, Item, Order}
 
   def call(params) do
     params
@@ -9,11 +9,20 @@ defmodule Rockelivery.Order.Create do
   end
 
   defp fetch_items(%{"items" => items_params}) do
-    item_ids = Enum.map(item_params, fn item -> item["id"] end)
+    item_ids = Enum.map(items_params, fn item -> item["id"] end)
 
     query = from item in Item, where: item.id in ^item_ids
 
     query
     |> Repo.all()
+    |> validate_items(item_ids)
+  end
+
+  defp validate_items(items, items_ids) do
+    items_map = Map.new(items, fn item -> {item.id, item} end)
+
+    items_ids
+    |> Enum.map(fn id -> {id, Map.get(items_map, id)} end)
+    |> Enum.any?(fn {_id,value} -> is_nil(value) end)
   end
 end
