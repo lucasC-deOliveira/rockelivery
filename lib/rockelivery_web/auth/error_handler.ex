9116019bca7bd2711bb69e60e4e5937402fabda0
Lucas Carvalho <67061874+lucasC-deOliveira@users.noreply.github.com>
2022-10-03 16:16:53 -0300

    Lidando com erros do pipeline de auth

--- lib/rockelivery_web/auth/error_handler.ex
+++ lib/rockelivery_web/auth/error_handler.ex
@@ -1,3 +1,13 @@
 defmodule RockeliveryWeb.Auth.ErrorHandler do
+  alias Guardian.Plug.ErrorHandler
 
+  alias Plug.Conn
+
+  @behaviour ErroHandler
+
+  def auth_error(conn, {error, _reason}, _opts) do
+    body = Jason.encode!(%{message: to_string(error)})
+
+    Conn.send_resp(conn, 401, body)
+  end
 end
