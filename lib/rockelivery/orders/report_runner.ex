a681853d46152f443fab0c12c39a563cc338237d
Lucas Carvalho <67061874+lucasC-deOliveira@users.noreply.github.com>
2022-10-04 15:00:45 -0300

    Agendando a geração de relatórios

---
+++ lib/rockelivery/orders/report_runner.ex
@@ -0,0 +1,33 @@
+defmodule Rockelivery.Orders.ReportRunner do
+  use GenServer
+
+  require Logger
+
+  alias Rockelivery.Orders.Report
+
+  def start_link(_initial_state) do
+    GenServer.start_link(__MODULE__, %{})
+  end
+
+  @impl true
+  def init(state) do
+    schedule_report_generation()
+    {:ok, state}
+  end
+
+  @impl true
+  # Recebe qualquer tipo de mensagem
+  def handle_info(:generate, state) do
+    Logger.info("Generating report...")
+
+    Report.create()
+
+    schedule_report_generation()
+
+    {:noreply, state}
+  end
+
+  def schedule_report_generation do
+    Process.send_after(self(), :generate, 1000 * 10)
+  end
+end
